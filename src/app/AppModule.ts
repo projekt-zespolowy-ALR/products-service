2d886a834969f0a1d1a1a18bc6934ddb2d5035de
Rafał Majewski <goodheropl@gmail.com>
2023-01-18 15:59:18 +0100

    Import AppConfigModule from index.js

--- src/app/AppModule.ts
+++ src/app/AppModule.ts
@@ -1,5 +1,5 @@
 import {Module} from "@nestjs/common";
-import AppConfigModule from "../app_config/AppConfigModule.js";
+import {AppConfigModule} from "../app_config/index.js";
 import {AppOrmModule} from "../app_orm/index.js";
 import {ProductsModule} from "../features/products/index.js";
 
