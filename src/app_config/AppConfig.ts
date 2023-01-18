9331f76ffd5cc21e266e405e6bb64b97cb1cb047
Rafał Majewski <goodheropl@gmail.com>
2023-01-18 17:26:33 +0100

    Explicit type annotations in AppConfig

--- src/app_config/AppConfig.ts
+++ src/app_config/AppConfig.ts
@@ -1,25 +1,32 @@
+import {Type} from "class-transformer";
 import {IsInt, IsString, Min, Max} from "class-validator";
 
 class AppConfig {
+	@Type(() => Number)
 	@IsInt()
 	@Min(1)
 	@Max(65535)
 	public readonly PORT: number = 3000;
 
+	@Type(() => String)
 	@IsString()
 	public readonly POSTGRES_HOST: string = "localhost";
 
+	@Type(() => Number)
 	@IsInt()
 	@Min(1)
 	@Max(65535)
 	public readonly POSTGRES_PORT: number = 5432;
 
+	@Type(() => String)
 	@IsString()
 	public readonly POSTGRES_USERNAME: string = "postgres";
 
+	@Type(() => String)
 	@IsString()
 	public readonly POSTGRES_PASSWORD: string = "postgres";
 
+	@Type(() => String)
 	@IsString()
 	public readonly POSTGRES_DATABASE: string = "postgres";
 }
