79d5f46918b4b08ced5d614afcdbaeeb04b229f7
Rafał Majewski <goodheropl@gmail.com>
2023-01-20 03:06:24 +0100

    Add AppConfigModule to ProjectsModule

--- src/features/products/ProductsModule.ts
+++ src/features/products/ProductsModule.ts
@@ -3,9 +3,10 @@ import ProductsController from "./ProductsController.js";
 import ProductsService from "./ProductsService.js";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import ProductEntity from "./ProductEntity.js";
+import AppConfigModule from "../../config/AppConfigModule.js";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([ProductEntity])],
+	imports: [TypeOrmModule.forFeature([ProductEntity]), AppConfigModule],
 	controllers: [ProductsController],
 	providers: [ProductsService],
 })
