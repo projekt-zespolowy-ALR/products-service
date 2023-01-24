7839f3610fcd6d6c2e746fd5185b7e699c0f5ad1
Rafał Majewski <goodheropl@gmail.com>
2023-01-24 19:34:37 +0100

    configureApp script use in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,12 @@
 import {NestFactory} from "@nestjs/core";
 import AppModule from "./AppModule.js";
 import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
+import configureApp from "./configureApp.js";
 import {AppConfig} from "./config/index.js";
-import {HttpStatus, ValidationPipe, VersioningType} from "@nestjs/common";
 
-const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
-	cors: {
-		methods: "*",
-		allowedHeaders: "*",
-		exposedHeaders: "*",
-		credentials: true,
-
-		origin: (requestOrigin, callback) => {
-			callback(null, true);
-		},
-	},
-});
-app.enableVersioning({
-	type: VersioningType.URI,
-	defaultVersion: "1",
-});
-app.useGlobalPipes(
-	new ValidationPipe({
-		transform: true,
-		whitelist: true,
-		errorHttpStatusCode: HttpStatus.BAD_REQUEST,
-	})
-);
+const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+configureApp(app);
 const appConfig = app.get(AppConfig);
+
 await app.listen(appConfig.PORT, appConfig.HOST);
 console.log(`Next.js server listening at ${await app.getUrl()}`);
