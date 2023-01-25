a9fdc7d7da3ae1a16ba5bf9eeab4d8d9828ff941
Rafał Majewski <goodheropl@gmail.com>
2023-01-25 09:08:38 +0100

    ref

--- src/features_module/FeaturesModule.ts
+++ src/features_module/FeaturesModule.ts
@@ -1,7 +1,7 @@
 import {Module} from "@nestjs/common";
 import BrandsModule from "../features/brands/brands_module/BrandsModule.js";
 import CategoriesModule from "../features/categories/categories_module/CategoriesModule.js";
-import DataSourcesModule from "../features/data_sources/DataSourcesModule.js";
+import DataSourcesModule from "../features/data_sources/data_sources_module/DataSourcesModule.js";
 import ProductsModule from "../features/products/products_module/ProductsModule.js";
 
 @Module({
