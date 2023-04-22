8f84dffb332ff3b4a98a94d7bcc04b1c77720fe2
Rafał Majewski <goodheropl@gmail.com>
2023-04-22 23:03:19 +0200

    Uninstall Nest.js swagger

--- src/paging/PagingOptions.ts
+++ src/paging/PagingOptions.ts
@@ -1,25 +1,13 @@
 import {IsInt, IsOptional, Max, Min} from "class-validator";
-import {ApiPropertyOptional} from "@nestjs/swagger";
 import {Type} from "class-transformer";
 
 class PagingOptions {
-	@ApiPropertyOptional({
-		default: 0,
-		minimum: 0,
-		type: Number,
-	})
 	@IsInt()
 	@Min(0)
 	@Type(() => Number)
 	@IsOptional()
 	public readonly skip = 0;
 
-	@ApiPropertyOptional({
-		default: 10,
-		minimum: 0,
-		maximum: 100,
-		type: Number,
-	})
 	@IsInt()
 	@Min(0)
 	@Max(100)
