6c8d68a188ac40486ca20f69c0c9942826f74f53
Rafał Majewski <goodheropl@gmail.com>
2023-04-22 15:51:26 +0200

    fixup

--- src/paging/Page.ts
+++ src/paging/Page.ts
@@ -1,9 +1,16 @@
 import PageMeta from "./PageMeta.js";
+import {plainToClass} from "class-transformer";
 
 class Page<T> {
 	public readonly items!: T[];
 
 	public readonly meta!: PageMeta;
+	public map<U>(mapper: (item: T) => U): Page<U> {
+		return plainToClass(Page, {
+			items: this.items.map(mapper),
+			meta: this.meta,
+		}) as Page<U>;
+	}
 }
 
 export default Page;
