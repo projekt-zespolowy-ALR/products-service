938f1b64382a8018d07397094f9e60edb568c16b
Rafał Majewski <goodheropl@gmail.com>
2023-05-14 21:42:25 +0200

    Fix missing IngredientsModule import in FeaturesModule

--- src/features/FeaturesModule.ts
+++ src/features/FeaturesModule.ts
@@ -1,9 +1,10 @@
 import {Module} from "@nestjs/common";
 import BrandsModule from "./brands/brands_module/BrandsModule.js";
 import CategoriesModule from "./categories/categories_module/CategoriesModule.js";
+import IngredientsModule from "./ingredients/ingredients_module/IngredientsModule.js";
 
 @Module({
-	imports: [BrandsModule, CategoriesModule],
+	imports: [BrandsModule, CategoriesModule, IngredientsModule],
 	controllers: [],
 	providers: [],
 })
