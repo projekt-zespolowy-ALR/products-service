c9014a34afe555ae99a26384a22aed8f21bf18f9
Rafał Majewski <goodheropl@gmail.com>
2023-05-14 17:42:11 +0200

    Fix get brand by id throwing not found error

--- src/features/brands/brands_service/BrandsService.ts
+++ src/features/brands/brands_service/BrandsService.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@nestjs/common";
-import {Repository} from "typeorm";
+import {EntityNotFoundError, Repository} from "typeorm";
 import BrandEntity from "./BrandEntity.js";
 import {InjectRepository} from "@nestjs/typeorm";
 import type Page from "../../../paging/Page.js";
@@ -8,6 +8,7 @@ import paginatedFindAndCount from "../../../paging/paginatedFindAndCount.js";
 import type Brand from "../brands_controller/Brand.js";
 import deentityifyBrandEntity from "./deentityifyBrandEntity.js";
 import type CreateBrandPayload from "./CreateBrandPayload.js";
+import BrandsServiceBrandWithGivenIdNotFoundError from "./BrandsServiceBrandWithGivenIdNotFoundError.js";
 @Injectable()
 export default class BrandsService {
 	private readonly brandsRepository: Repository<BrandEntity>;
@@ -20,7 +21,14 @@ export default class BrandsService {
 		);
 	}
 	public async getBrandById(id: string): Promise<Brand> {
-		return deentityifyBrandEntity(await this.brandsRepository.findOneByOrFail({id}));
+		try {
+			return deentityifyBrandEntity(await this.brandsRepository.findOneByOrFail({id}));
+		} catch (error) {
+			if (error instanceof EntityNotFoundError) {
+				throw new BrandsServiceBrandWithGivenIdNotFoundError(id);
+			}
+			throw error;
+		}
 	}
 	public async createBrand(createBrandPayload: CreateBrandPayload): Promise<Brand> {
 		return deentityifyBrandEntity(await this.brandsRepository.save(createBrandPayload));
