8b54f22a4776515155c629ba4e0ea3927e5fece9
Ludwika Malinowska <malinowska.L@wp.pl>
2023-06-21 23:27:22 +0200

    Eslint fix

--- src/features/products/products_service/ProductsService.ts
+++ src/features/products/products_service/ProductsService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {Injectable} from "@nestjs/common";
 import {EntityNotFoundError, Repository} from "typeorm";
 import ProductEntity from "./ProductEntity.js";
